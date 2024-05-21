edc39144262dba1d8d758c0e1acb9d0a70f534b5
terra mauthe <terra.mauthe@grafana.com>
2024-05-21 16:15:02 -0700

    feat: Implement probe binary
    Flags are hard coded for now; they are being worked on in parallel.
    Simple probe connects to an address and reports success or failure.

--- cmd/probe/main.go
+++ cmd/probe/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
 
+func main() {
+	// TODO: FLAGS
+	timeout := 5 * time.Second
+	host := "grafana.com"
+	port := "443"
+	expectFail := false
+
+	// Make TCP connection
+	address := fmt.Sprintf("%s:%s", host, port)
+	conn, err := net.DialTimeout("tcp", address, timeout)
+	// Handle connection failure
+	if err != nil {
+		fmt.Println("Encountered an error trying to create network connection", err)
+		// Exit 0 if the connection had the expected result
+		// note: sometimes, failure to connect is the expected result
+		if expectFail {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("Connection succeeded to:", conn.RemoteAddr().String(), fmt.Sprintf("(%s)", address))
+	err = conn.Close()
+	if err != nil {
+		fmt.Println("Encountered error trying to close TCP connection:", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
