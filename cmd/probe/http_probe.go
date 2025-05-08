9915db467caa6580cb98056003732ae8905e3f6f
Leandro López (inkel) <leandro.lopez@grafana.com>
2025-05-08 15:41:31 -0300

    Add HTTP probe
    Implement a simple HTTP probe that passes the Probe interface.

---
+++ cmd/probe/http_probe.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var _ Probe = &HTTPProbe{}
+
+type HTTPProbe struct {
+	url       string
+	timeout   time.Duration
+	resStatus int
+	expStatus int
+	err       error
+}
+
+func NewHTTPProbe(url string, timeout time.Duration, expectedStatus int) *HTTPProbe {
+	return &HTTPProbe{
+		url:       url,
+		timeout:   timeout,
+		expStatus: expectedStatus,
+	}
+}
+
+func (p *HTTPProbe) Run() {
+	c := &http.Client{
+		Timeout: timeout,
+	}
+
+	res, err := c.Get(p.url)
+	if err != nil {
+		p.err = err
+		return
+	}
+	defer res.Body.Close() //nolint:errcheck
+
+	p.resStatus = res.StatusCode
+}
+
+func (p *HTTPProbe) Assert() error {
+	if p.expStatus == 0 && p.err == nil {
+		return errConnectionSucceeded
+	} else if p.expStatus != 0 && p.err != nil {
+		return fmt.Errorf("%w: %w", errConnectionFailed, p.err)
+	} else if p.expStatus != p.resStatus {
+		return fmt.Errorf("%w: expecting response code %d, got %d", errAssertionFailed, p.expStatus, p.resStatus)
+	}
+
+	return nil
+}
---
+++ cmd/probe/http_probe_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestHTTPProbe(t *testing.T) {
+	t.Run("expecting status code", func(t *testing.T) {
+		status := http.StatusOK
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(status)
+		}))
+
+		p := NewHTTPProbe(ts.URL, time.Second, status)
+		p.Run()
+
+		if err := p.Assert(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("expecting connection failure", func(t *testing.T) {
+		ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		}))
+
+		p := NewHTTPProbe(ts.URL, time.Second, 0)
+		p.Run()
+
+		if err := p.Assert(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("connection succeeded", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		p := NewHTTPProbe(ts.URL, time.Second, 0)
+		p.Run()
+
+		if err := p.Assert(); !errors.Is(err, errConnectionSucceeded) {
+			t.Fatalf("expecting error %v, got %v", errConnectionSucceeded, err)
+		}
+	})
+
+	t.Run("unexpected response", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}))
+
+		p := NewHTTPProbe(ts.URL, time.Second, http.StatusOK)
+		p.Run()
+
+		if err := p.Assert(); !errors.Is(err, errAssertionFailed) {
+			t.Fatalf("expecting error %v, got %v", errAssertionFailed, err)
+		}
+	})
+
+	t.Run("connection failure", func(t *testing.T) {
+		ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}))
+
+		p := NewHTTPProbe(ts.URL, time.Second, http.StatusOK)
+		p.Run()
+
+		if err := p.Assert(); !errors.Is(err, errConnectionFailed) {
+			t.Fatalf("expecting error %v, got %v", errConnectionFailed, err)
+		}
+	})
+}
