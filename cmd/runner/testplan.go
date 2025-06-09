24c858dce486801a168f7275e19038f63ae47100
Leandro López <leandro.lopez@grafana.com>
2025-06-09 08:06:02 -0300

    feat: use custom YAML parsing for enum types (#98)

--- cmd/runner/testplan.go
+++ cmd/runner/testplan.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"strings"
 	"time"
@@ -13,7 +15,7 @@ type Test struct {
 	Name       string        `yaml:"name"`
 	Endpoint   string        `yaml:"endpoint"`
 	StatusCode int           `yaml:"statusCode"`
-	Type       string        `yaml:"type,omitempty"`
+	Type       TestType      `yaml:"type,omitempty"`
 	ExpectFail bool          `yaml:"expectFail,omitempty"`
 	Timeout    time.Duration `yaml:"timeout"`
 	ProbeImage string        `yaml:"probeImage,omitempty"`
@@ -21,15 +23,15 @@ type Test struct {
 
 // PodSelector represents how pods should be selected for testing
 type PodSelector struct {
-	Mode   string `yaml:"mode"` // "all" or "random"
-	Labels string `yaml:"labels"`
-	Fields string `yaml:"fields"`
+	Mode   SelectionMode `yaml:"mode"` // "all" or "random"
+	Labels string        `yaml:"labels"`
+	Fields string        `yaml:"fields"`
 }
 
 func (s PodSelector) String() string {
 	var b strings.Builder
 	b.WriteString("mode: ")
-	b.WriteString(s.Mode)
+	b.WriteString(string(s.Mode))
 
 	if s.Labels != "" {
 		b.WriteString(", labels: ")
@@ -67,13 +69,68 @@ func ParseTestPlan(reader io.Reader) (*TestPlan, error) {
 		return nil, err
 	}
 
-	// Set default test type to HTTP(S)
-	for i, target := range plan.TestPlan.TestTargets {
-		for j, test := range target.Tests {
-			if test.Type == "" {
-				plan.TestPlan.TestTargets[i].Tests[j].Type = "HTTP(S)"
-			}
-		}
-	}
 	return &plan.TestPlan, nil
+}
+
+type TestType int
+
+func (tt TestType) String() string {
+	switch tt {
+	case TestTypeHTTP:
+		return "http"
+	case TestTypeTCP:
+		return "tcp"
+	default:
+		panic("unrecognized testype")
+	}
+}
+
+const (
+	TestTypeHTTP TestType = iota
+	TestTypeTCP
+)
+
+var errInvalidTestType = errors.New("invalid test type")
+
+func yamlUnmarshalTestType(tt *TestType, b []byte) error {
+	switch strings.TrimSpace(strings.ToLower(string(b))) {
+	case "", "http", "https":
+		*tt = TestTypeHTTP
+	case "tcp":
+		*tt = TestTypeTCP
+	default:
+		return fmt.Errorf("%w: %q", errInvalidTestType, b)
+	}
+
+	return nil
+}
+
+func init() {
+	yaml.RegisterCustomUnmarshaler(yamlUnmarshalTestType)
+	yaml.RegisterCustomUnmarshaler(yamlUnmarshalSelectionMode)
+}
+
+type SelectionMode string
+
+const (
+	SelectionModeAll    SelectionMode = "all"
+	SelectionModeRandom SelectionMode = "random"
+)
+
+func yamlUnmarshalSelectionMode(m *SelectionMode, b []byte) error {
+	// we need to add the cases with quotes because YAML is such a
+	// good language that `foo: bar` is the same as `foo: "bar"` and
+	// also the same as `foo: 'bar'` but, hey, the three values are
+	// passed as-is to the parser and thus have to take into account
+	// these characters.
+	switch strings.TrimSpace(strings.ToLower(string(b))) {
+	case "all", "\"all\"", "'all'":
+		*m = SelectionModeAll
+	case "random", "\"random\"", "'random'":
+		*m = SelectionModeRandom
+	default:
+		return fmt.Errorf("%w: %q", errInvalidSelectionMode, b)
+	}
+
+	return nil
 }
--- cmd/runner/testplan_test.go
+++ cmd/runner/testplan_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"errors"
 	"fmt"
 	"strings"
 	"testing"
@@ -26,7 +27,7 @@ func TestParseTestPlan(t *testing.T) {
 		tt := tp.TestTargets[0]
 
 		for i, tt := range tt.Tests {
-			if tt.Type != "HTTP(S)" {
+			if tt.Type != TestTypeHTTP {
 				t.Errorf("expecting test target 0, test %d to be of type HTTP(S), got %q", i, tt.Type)
 			}
 		}
@@ -35,7 +36,7 @@ func TestParseTestPlan(t *testing.T) {
 	t.Run("don't override type", func(t *testing.T) {
 		tt := tp.TestTargets[1].Tests[1]
 
-		if e, g := "tcp", tt.Type; e != g {
+		if e, g := TestTypeTCP, tt.Type; e != g {
 			t.Fatalf("expecting type to be %q, got %q", e, g)
 		}
 	})
@@ -77,5 +78,76 @@ func TestPodSelector_String(t *testing.T) {
 		if got := fmt.Sprint(tt.sel); tt.exp != got {
 			t.Errorf("for %#v expecting %q, got %q", tt.sel, tt.exp, got)
 		}
+	}
+}
+
+func TestTestType(t *testing.T) {
+	// We default to HTTP
+	var tt TestType
+
+	if tt != TestTypeHTTP {
+		t.Fatalf("unexpected zero value: %d", tt)
+	}
+}
+
+func TestTestType_UnmarshalYAML(t *testing.T) {
+	tests := map[string]struct {
+		exp TestType
+		err error
+	}{
+		"":      {TestTypeHTTP, nil},
+		"http":  {TestTypeHTTP, nil},
+		"https": {TestTypeHTTP, nil},
+		"tcp":   {TestTypeTCP, nil},
+		// ignore case
+		"HTTP":  {TestTypeHTTP, nil},
+		"HTTPS": {TestTypeHTTP, nil},
+		"TCP":   {TestTypeTCP, nil},
+		// invalid values // TODO(inkel) this could probably be a fuzz test
+		"foo": {TestTypeHTTP, errInvalidTestType},
+	}
+
+	for in, tt := range tests {
+		t.Run("in="+in, func(t *testing.T) {
+			var got TestType
+
+			err := yamlUnmarshalTestType(&got, []byte(in))
+			if !errors.Is(err, tt.err) {
+				t.Fatalf("expecting error %v, got %v", tt.err, err)
+			}
+			if tt.exp != got {
+				t.Fatalf("expecting TestType %d (%[1]s) got %d (%[2]s)", tt.exp, got)
+			}
+		})
+	}
+}
+
+func TestSelectionMode_UnmarshalYAML(t *testing.T) {
+	tests := map[string]struct {
+		exp SelectionMode
+		err error
+	}{
+		// cannot be empty
+		"": {"", errInvalidSelectionMode},
+
+		"all": {SelectionModeAll, nil},
+		"ALL": {SelectionModeAll, nil},
+
+		"random": {SelectionModeRandom, nil},
+		"rAnDom": {SelectionModeRandom, nil},
+	}
+
+	for in, tt := range tests {
+		t.Run("in="+in, func(t *testing.T) {
+			var got SelectionMode
+
+			err := yamlUnmarshalSelectionMode(&got, []byte(in))
+			if !errors.Is(err, tt.err) {
+				t.Fatalf("expecting error %v, got %v", tt.err, err)
+			}
+			if tt.exp != got {
+				t.Fatalf("expecting SelectionMode %q, got %q", tt.exp, got)
+			}
+		})
 	}
 }
